6111ed23f12ccaa6f18b16c13869266811410216
preet2542 <gurpreet.singh0512@gmail.com>
2018-03-10 17:21:07 -0500

    Add files via upload

---
+++ CLASSACTIVITY/src/classactivity/CLASSACTIVITY.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classactivity;
+
+/**
+ *
+ * @author GURPREET SINGH
+ */
+public class CLASSACTIVITY {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // TODO code application logic here
+         for (int x = 0; x < 100; x++)
+    
+   
+
+
+
+            {
+
+
+
+                if (x % 5 == 0)
+
+                {
+
+                   System.out.println("wouf");
+
+                }
+
+
+
+                else if (x % 3 == 0 )
+
+                {
+
+                   System.out.println(" meow ");
+
+
+
+                }
+
+
+
+             
+
+                else if (x % 3 == 0 && x % 5 == 0)
+
+                {
+
+
+                   System.out.println(" moo ");
+
+                 }
+
+
+
+                else
+
+                {
+                   System.out.println("" + x + " ");
+
+
+
+                }
+            }}}
+    
+          
+
+        
+
+
+
+    
+
+
+    
+
